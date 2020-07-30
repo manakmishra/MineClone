4dbb7a4a28231855e6fbfc6244f73be5a868f783
Manak Mishra <18ucc104@lnmiit.ac.in>
2020-07-30 13:11:35 +0530

    first voxel created

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+    void Start()
+    {
+        int vertIndex = 0;
+        List<Vector3> vertices = new List<Vector3> ();
+        List<int> triangles = new List<int> ();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for(int i=0; i<6; i++) {
+            for(int j=0; j<6; j++) {
+                int triangleIndex = VoxelData.voxelTriangles[i, j];
+                vertices.Add(VoxelData.voxelVert[triangleIndex]);
+                triangles.Add(vertIndex);
+                uvs.Add(VoxelData.voxeluvs[j]);
+
+                vertIndex++;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
+
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+    }
+}
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData
+{
+    public static readonly Vector3[] voxelVert = new Vector3[8] {
+        new Vector3(0.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 1.0f),
+    };
+
+    public static readonly int[,] voxelTriangles = new int[6, 6] {
+        {0, 3, 1, 1, 3, 2}, //backFace
+        {5, 6, 4, 4, 6, 7}, //frontFace
+        {3, 7, 2, 2, 7, 6}, //topFace
+        {1, 5, 0, 0, 5, 4}, //bottomFace
+        {4, 7, 0, 0, 7, 3}, //leftFace
+        {1, 2, 5, 5, 2, 6}, //rightFace  
+    };
+
+    public static readonly Vector2[] voxeluvs = new Vector2[6] {
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(1.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 1.0f),
+    };
+
+}
